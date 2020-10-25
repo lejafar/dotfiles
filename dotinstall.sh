39df2f08b8b08f25ffc1fd902f92c697eefe8fab
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2020-10-25 15:41:43 +0100

    update latest changes

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y git ruby
+sudo apt-get install -y git ruby tmux zsh
+
+# homesick dotfiles
 sudo gem install homesick
 homesick clone lejafar/dotfiles && homesick symlink dotfiles --force
 homesick clone lejafar/dotvim && homesick symlink dotvim
-
-# install YouCompleteMe
-sudo apt-get install -y python-dev
-~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-
-source ~/.bashrc
-sudo apt-get install -y exuberant-ctags silversearcher-ag software-properties-common
-
-# tmux
-sudo apt-get install tmux
 
 # vim 8
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get install -y vim
+vim +PluginInstall +qall # install Vundle plugins
+
+# set up zsh & prezto
+zsh
+git clone --recursive https://github.com/lejafar/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+source ~/.zshrc
+chsh -s /bin/zsh
 
 # pyenv
 curl https://pyenv.run | bash
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
