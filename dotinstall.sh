ca2a79948823b909e4bfd1cd0a496a9821ea6726
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2020-10-25 17:15:44 +0100

    fix install script

--- dotinstall.sh
+++ dotinstall.sh
@@ -16,7 +16,7 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 source ~/.zshrc
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.4
 source $HOME/.poetry/env
@@ -25,13 +25,13 @@ source $HOME/.poetry/env
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get install -y vim
-vim +PluginInstall +qall || true # install Vundle plugins
 
 # prettier install
 sudo apt-get install -y nodejs npm
-pushd ~/.vim/bundle/vim-prettier
-npm install
-popd
+sudo npm install -g prettier
 
 # install pyenv
 curl https://pyenv.run | bash
+
+# install Vundle plugins
+vim +'PlugInstall --sync' +qa || true
