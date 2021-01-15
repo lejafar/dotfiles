278c31c49a2ed84a4208c28c5b20082005e7c1d3
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2021-01-15 09:38:23 +0100

    Update dotinstall.sh

--- dotinstall.sh
+++ dotinstall.sh
@@ -7,6 +7,12 @@ sudo gem install homesick
 homesick clone lejafar/dotfiles && homesick symlink dotfiles --force
 homesick clone lejafar/dotvim && homesick symlink dotvim
 
+# install pyenv
+curl https://pyenv.run | bash
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.4
+source $HOME/.poetry/env
+
 # set up zsh & prezto
 sudo apt-get install -y zsh
 zsh
@@ -16,22 +22,13 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 source ~/.zshrc
-sudo chsh -s /bin/zsh
+chsh -s /bin/zsh
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.4
-source $HOME/.poetry/env
+# install Vundle plugins
+vim +PluginInstall +qall
 
 # vim 8
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get install -y vim
 
-# prettier install
-sudo apt-get install -y nodejs npm
-sudo npm install -g prettier
-
-# install pyenv
-curl https://pyenv.run | bash
-
-# install Vundle plugins
-vim +PluginInstall +qall
